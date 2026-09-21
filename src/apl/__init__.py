aeae5c53323dde13fde7e86079172d4f6bdeea31
perdix1975 <75627997+perdix1975@users.noreply.github.com>
2026-09-22 01:08:41 +0300

    feat: add APL 0.0.3 language version

--- src/apl/__init__.py
+++ src/apl/__init__.py
@@ -1,6 +1,6 @@
 """APL reference implementation."""
 
-LATEST_LANGUAGE_VERSION = "0.0.2"
-SUPPORTED_LANGUAGE_VERSIONS = {"0.0.1", "0.0.2"}
+LATEST_LANGUAGE_VERSION = "0.0.3"
+SUPPORTED_LANGUAGE_VERSIONS = {"0.0.1", "0.0.2", "0.0.3"}
 LANGUAGE_VERSION = LATEST_LANGUAGE_VERSION
-__version__ = "0.0.2"
+__version__ = "0.0.3"
